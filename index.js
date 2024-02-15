7d42b5768ae670847e3e152783ba70a4bd7c9164
JunoPrice <jonas.3636@hotmail.com>
2024-02-15 02:20:14 -0300

    Final do segundo desafio Felipão da DIO

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const entrada = require('prompt-sync')({sigint: true});
-const notificacao = "Warcraft diz: ";
+const notificacao = "LoL diz: ";
 
-console.log(notificacao + "Bem vindo ao mundo de Warcraft");
+console.log(notificacao + "Bem vindo a League of Legends, prepare-se para provar seu valor");
 
-    let nomeheroi = entrada(notificacao + "Heroi, qual é seu nome? ");
-    let xpheroi= entrada(notificacao + "Digite seu nível de experiência como heroi: ");
-    let nivelheroi = ["Ferro" , "Bronze" , "Prata" , "Ouro" , "Platina" , "Ascendente" , "Imortal" , "Radiante"];
+    let  summonerName= entrada(notificacao + "Insira seu nome de invocador: ");
+    let nivelheroi = ["Ferro" , "Bronze" , "Prata" , "Ouro" , "Diamante" , "Lendário" , "Imortal"];
     
-console.log(notificacao + "Bem vindo(a): " + nomeheroi);
-console.log(notificacao + nomeheroi + " entrou no mundo de Warcraft");
-if(xpheroi <= 1000 ){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[0])
-}else if(xpheroi <= 2000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[1])
-}else if(xpheroi <= 6000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[2])
-}else if(xpheroi <= 7000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[3])
-}else if(xpheroi <= 8000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[4])
-}else if(xpheroi <= 9000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[5])
-}else if(xpheroi <= 10000){
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[6])
+console.log(notificacao + "Bem vindo(a): " + summonerName);
+console.log(notificacao + summonerName + " entrou em Summoner's Rift");
+
+
+let summonerWin= entrada(notificacao + "Insira sua quantidade de vitórias Invocador: ");
+let summonerLoss= entrada(notificacao + "Insira sua quantidade de derrotas Invocador: ");
+
+let summonerRank = soma(parseInt(summonerWin), parseInt(summonerLoss)) /*foi necessário usar o parseInt pois a função concatenava os valores
+ de vitórias e derrotas ao invés de subtrair, pois o considerava uma string*/
+function soma(numA, numB){
+    let somatorio = numA - numB
+    return somatorio
+}
+console.log(notificacao + summonerName +", o Saldo de suas partidas rankeadas é: " + summonerRank)
+
+if(summonerRank <= 10 ){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[0])
+}else if(summonerRank <= 20){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[1])
+}else if(summonerRank <= 50){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[2])
+}else if(summonerRank <= 80){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[3])
+}else if(summonerRank <= 90){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[4])
+}else if(summonerRank <= 100){
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[5])
 }else{
-        console.log(notificacao + nomeheroi + ", seu Nível de heroi é: " + nivelheroi[7])
+        console.log(notificacao + summonerName + ", seu nível de partidas rankeadas é: " + nivelheroi[6])
 };
-console.log(notificacao + "O mapa será atribuido de acordo com seu nível, contactando servidores, aguarde...");
+
+console.log(notificacao + "As partidas rankeadas serão atribuidas de acordo com seu rank, contactando servidores, aguarde...");
